1bbeecd27f3669c3c14ffc073cfa044af4d6cb2a
Owen Klan <owen.j.klan@gmail.com>
2024-12-22 14:19:59 +1000

    Finish implementing the Auto-outline plugin

--- scripts/auto_outline/auto_outline.py
+++ scripts/auto_outline/auto_outline.py
@@ -14,12 +14,13 @@
 # ie: Able to be bound to a shortcut / Stream Deck button.
 
 import sys
+import time
 
 import gi
+
 gi.require_version('Gimp', '3.0')
 from gi.repository import Gimp
 gi.require_version('GimpUi', '3.0')
-from gi.repository import GimpUi
 from gi.repository import GLib
 
 
@@ -49,8 +50,50 @@ class AutoOutline (Gimp.PlugIn):
         return procedure
 
     def run(self, procedure, run_mode, image, drawables, config, run_data):
-        Gimp.message("Hello world!")
-        # do what you want to do, then, in case of success, return:
+        selected_layers = image.get_selected_layers()
+
+        if len(selected_layers) != 1:
+            Gimp.message("Auto-outline only works for a single layer.")
+            return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
+
+        # Push the current context
+        if not Gimp.context_push():
+            Gimp.message("Auto-outline failed to push context!")
+            return procedure.new_return_values(Gimp.PDBStatusType.FAILED, GLib.Error())
+
+        layer = selected_layers[0]
+
+        # As of 22nd Dec. 2024, it appears that Gimp.Unit.pixel() is broken.
+        # So, we determine the pixels to points then set the line width unit
+        # to points
+        LINE_WIDTH_IN_PIXELS = 6.0
+
+        # We need to obtain the image DPI for the pixels-to-points conversion
+        _, x_resolution, y_resolution = image.get_resolution()
+        line_width_in_points = Gimp.pixels_to_units(LINE_WIDTH_IN_PIXELS, Gimp.Unit.point(), x_resolution)
+
+        # Grow the selection by half the width of the stroke
+        image_selection = image.get_selection()
+        image_selection.grow(image, LINE_WIDTH_IN_PIXELS / 2)
+
+        # Paint a stroke along the selection
+        Gimp.context_set_line_width_unit(Gimp.Unit.point())
+        Gimp.context_set_line_width(
+            line_width_in_points,
+        )
+        Gimp.context_set_antialias(True)
+        Gimp.context_set_brush_hardness(1.0)
+        Gimp.context_set_stroke_method(Gimp.StrokeMethod.LINE)
+
+        # Actually perform the stroke
+        layer.edit_stroke_selection()
+
+        # Pop the context back off the stack
+        Gimp.context_pop()
+
+        success_message = f"Applied automatic outline of {line_width_in_points} points ({LINE_WIDTH_IN_PIXELS} pixels)."
+        Gimp.message(success_message)
+
         return procedure.new_return_values(Gimp.PDBStatusType.SUCCESS, GLib.Error())
 
 
